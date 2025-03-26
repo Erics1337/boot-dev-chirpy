8f8195d07e5be80f4902f09691f98d93a06ac42c
Eric <erics1337@gmail.com>
2025-03-26 17:32:11 -0600

    initialize Go module and add basic HTTP server implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ericswanson/boot-dev-chirpy
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Create a new ServeMux
+	mux := http.NewServeMux()
+
+	// Create the server with the mux as handler
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	// Start the server
+	log.Printf("Server starting on %s", server.Addr)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
