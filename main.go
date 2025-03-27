8d5facd55e560d6226e52559b1e417e700114407
Eric <erics1337@gmail.com>
2025-03-26 22:26:29 -0600

    add HTML welcome page and serve static files from the current directory

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ func main() {
 	// Create a new ServeMux
 	mux := http.NewServeMux()
 
+	// Create a file server handler for the current directory
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/", fileServer)
+
 	// Create the server with the mux as handler
 	server := &http.Server{
 		Addr:    ":8080",
